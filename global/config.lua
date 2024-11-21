a00ce575c75e2f6ca4ccb853284c0b85eb91be3d
Nishant Joshi <nishantjo.12@gmail.com>
2024-11-22 02:11:35 +0530

    decoupling themes

--- global/config.lua
+++ global/config.lua
@@ -3,7 +3,7 @@ local colors = require "themes.colors"
 
 return function(config)
   config.font = wezterm.font_with_fallback {
-    { family = "Geist Mono",         weight = "Regular" },
+    { family = "Geist Mono",         weight = "Medium" },
     { family = "SFMono Nerd Font",   weight = "Medium" },
     { family = "Monaspace Neon Var", weight = "Bold" },
   }
--- themes/colors.lua
+++ themes/colors.lua
@@ -1,34 +1,12 @@
-local colors = {
-  foreground = "#c9c7cd",
-  background = "#161617",
-  cursor_bg = "#c9c7cd",
-  cursor_fg = "#161617",
-  cursor_border = "#c9c7cd",
-  selection_bg = "#2a2a2c",
-  selection_fg = "#c9c7cd",
-  ansi = {
-    "#27272a",
-    "#ea83a5",
-    "#90b99f",
-    "#e6b99d",
-    "#92a2d5",
-    "#e29eca",
-    "#85b5ba",
-    "#313134",
-  },
-  brights = {
-    "#353539",
-    "#ed96b3",
-    "#a7c8b3",
-    "#eac5ae",
-    "#a7b3dd",
-    "#e8b0d4",
-    "#97c0c4",
-    "#3b3b3e",
-  },
-  tab_bar = {
-    background = "#161617",
-  }
+local themes = {
+  ["kanagawa-dragon"] = require("themes.kanagawa-dragon"),
+  ["oldworld"] = require("themes.oldworld")
 }
 
-return colors
+local current_hour = tonumber(os.date("%H"));
+
+if current_hour >= 6 and current_hour < 19 then
+  return themes["oldworld"]
+else
+  return themes["kanagawa-dragon"]
+end
