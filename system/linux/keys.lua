4f1ea4a20386ff01bcf2ade5e13370401cac95d7
Nishant Joshi <nishantjo.12@gmail.com>
2024-12-26 00:27:17 +0530

    feat(keys): add directional splitting keybinds for linux

--- system/linux/keys.lua
+++ system/linux/keys.lua
@@ -13,5 +13,62 @@ return {
         key = "k",
         mods = "OPT",
         action = action.ClearScrollback 'ScrollbackAndViewport',
-    }
+    },
+
+    -- create directioanl splits
+    {
+        key = "j",
+        mods = "OPT|SHIFT",
+        action = action.SplitPane {
+            direction = 'Down',
+            size = { Percent = 50 },
+            command = {
+                domain = "CurrentPaneDomain",
+            },
+        }
+    },
+    {
+        key = "l",
+        mods = "OPT|SHIFT",
+        action = action.SplitPane {
+            direction = 'Right',
+            size = { Percent = 50 },
+            command = {
+                domain = "CurrentPaneDomain",
+            },
+        }
+    },
+    {
+        key = "h",
+        mods = "OPT|SHIFT",
+        action = action.SplitPane {
+            direction = 'Left',
+            size = { Percent = 50 },
+            command = {
+                domain = "CurrentPaneDomain",
+            },
+        }
+    },
+    {
+        key = "k",
+        mods = "OPT|SHIFT",
+        action = action.SplitPane {
+            direction = 'Up',
+            size = { Percent = 50 },
+            command = {
+                domain = "CurrentPaneDomain",
+            },
+        }
+    },
+
+    {
+        key = "h",
+        mods = "OPT",
+        action = wezterm.action.ActivatePaneDirection "Prev"
+    },
+    {
+        key = "l",
+        mods = "OPT",
+        action = wezterm.action.ActivatePaneDirection "Next"
+    },
 }
