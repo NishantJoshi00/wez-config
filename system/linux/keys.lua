dad44d2da02c2d32b6904d4297c5d375d1ec4bff
Nishant Joshi <nishantjo.12@gmail.com>
2024-12-26 00:21:38 +0530

    feat(global): add support for jetbrains font

--- system/linux/keys.lua
+++ system/linux/keys.lua
@@ -2,11 +2,16 @@ local wezterm = require('wezterm');
 local action = wezterm.action
 
 return {
-  {
-    key = "l",
-    mods = 'OPT',
-    action = action.SplitHorizontal {
-      domain = "CurrentPaneDomain"
+    {
+        key = "l",
+        mods = 'OPT',
+        action = action.SplitHorizontal {
+            domain = "CurrentPaneDomain"
+        }
+    },
+    {
+        key = "k",
+        mods = "OPT",
+        action = action.ClearScrollback 'ScrollbackAndViewport',
     }
-  }
 }
