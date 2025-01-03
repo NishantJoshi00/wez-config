da6a11b3b1a9c2ba18d9d0f3ac47c367e0c7c055
Nishant Joshi <nishantjosh@pm.me>
2025-01-04 02:39:03 +0530

    merge: fix stash

--- system/linux/config.lua
+++ system/linux/config.lua
@@ -1,10 +1,19 @@
 local utils = require 'utils'
 
 return function(config)
+    config.tab_bar_at_bottom = false
     config.enable_wayland = false
-    config.font_size = 14
+    config.font_size = 12
+
+    config.window_padding = {
+        left = '1cell',
+        right = '1cell',
+        top = 0,
+        bottom = 0
+    }
 
     config.front_end = "OpenGL"
+    config.use_ime = false
 
     return config
 end
