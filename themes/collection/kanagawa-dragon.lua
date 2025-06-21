cd4a62a2a48ad67558639a24ffe5e0e2c2fbfe94
Nishant Joshi <nishant.joshi@juspay.in>
2025-06-21 09:07:14 -0600

    chore: fix the theme

--- themes/collection/kanagawa-dragon.lua
+++ themes/collection/kanagawa-dragon.lua
@@ -3,32 +3,39 @@ local colors = {
     background = "#181616",
     cursor_bg = "#c5c9c5",
     cursor_fg = "#181616",
-    cursor_border = "#c5c9c5",
-    selection_bg = "#223249",
+    cursor_border = "#d0d4ce",
+    selection_bg = "#2a354a",
     selection_fg = "#c5c9c5",
+
     ansi = {
         "#0d0c0c", -- black
-        "#c4746e", -- red
+        "#b86868", -- red
         "#8a9a7b", -- green
         "#c4b28a", -- yellow
         "#8ba4b0", -- blue
         "#a292a3", -- magenta
-        "#8ea4a2", -- cyan
+        "#80a9ac", -- cyan
         "#c8c093", -- white
     },
+
     brights = {
         "#555555", -- bright black
-        "#e46876", -- bright red
+        "#d86b6f", -- bright red
         "#87a987", -- bright green
         "#e6c384", -- bright yellow
         "#7fb4ca", -- bright blue
         "#938aa9", -- bright magenta
-        "#7aa89f", -- bright cyan
+        "#6fa69c", -- bright cyan
         "#c5c9c5", -- bright white
     },
+
     tab_bar = {
         background = "#181616",
-    }
+        active_tab = {
+            bg_color = "#1e1b1a",
+            fg_color = "#c5c9c5",
+        },
+    },
 }
 
 return colors
